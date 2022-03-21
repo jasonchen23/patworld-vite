08896de43524b7f19655047783bedb32a7eb9b28
rayrayfan <kariforbiz@gmail.com>
2022-03-21 16:39:38 +0800

    0321

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,26 @@
+<template>
+
+<div class="container">
+    <div class="row">
+        <div class="d-flex card">
+            <img src="https://4.bp.blogspot.com/-Hbl_buIRu5o/XG4GXOwvXLI/AAAAAAABRq0/hEWfV0FE5IEG66wOidqBlI5IA0e28QFGgCLcBGAs/s400/ahiruguchi_man.png" alt="">
+            <h3>{{memberlist}}</h3>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+
+//export default {
+
+// }
+</script>
+
+<style>
+
+</style>
+
+
+  
--- src/views/MemberPage.vue
+++ src/views/MemberPage.vue
@@ -1,19 +1,38 @@
 <template> <!--開發人員頁面，若要從資料庫拿開發人員資料，則做成Component；無則直接以此頁面寫下去即可 -->
     <Navbar />
-    <div class="container">
-        <div class="row"><!--4人可做成兩種風格做穿插 -->
-            <div class="d-flex card"></div>
-        </div>
-    </div>
+    <MemberCard />
+    
 </template>
 
-<script setup>
-import Navbar from '@/components/NavbarBox.vue';
+
+
+
 // export default {
 
 // }
 </script>
+<script>
+import Navbar from '@/components/NavbarBox.vue';
+import MemberCard from '@/components/MemberCard.vue';
 
+export default {
+    components:{
+        MemberCard,Navbar
+    },
+    data(){
+        return{
+            memberlist:[
+                {}
+            ],
+
+
+        }
+    },
+    mounted(){
+
+    }
+}
+</script>
 <style>
 
 </style>
