7bc508ced869dc68a8d9ea5f672e04a2964959cc
Jasonchen23 <kevin3121961@gmail.com>
2022-03-29 09:46:23 +0800

    save

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -1,11 +1,12 @@
 <template>
   <!-- <Navbar /> -->
-  <div class="bg">
-  <div class="mx-5 d-flex">
-    <div class="img">
-      <router-link to="/adopt" class="home">
+  <!-- <div class="bg"> -->
+  <div class="mx-5 d-flex row">
+      <router-link to="/adopt" class="img col">
         <img src="../HomePage/img/tatemono_myhome.png" alt="">
       </router-link>
+    <div class="img p-0">
+      <p class="p">Lorem ipsum dolor sit amet.</p>
     </div>
     <div class="img">
       <router-link to="/donate" class="home">
@@ -28,29 +29,58 @@
       </router-link>
     </div>
   </div>
-  </div>
+  <div class="box"></div>
+  <img src="../HomePage/img/bg_chiheisen_green.jpg" alt="" class="bg">
+  <!-- </div> -->
 </template>
 
 <script>
 import Navbar from '@/components/NavbarBox.vue';
 export default {
 
-}
+};
+
+</script>
+<script setup>
+$(".p").hide();
+
+// $(".home").mouseenter(
+//   function fadein() {
+//     $("p").fadeIn("slow");
+//   }
+// );
+// $(".home").mouseleave(
+//   function fadeout() {
+//     $("p").fadeOut("slow");
+//   }
+// );
 </script>
 
 <style scoped>
-.bg{
+/* .bg{
     background-image: url('../HomePage/img/bg_chiheisen_green.jpg');
 
-  /* Full height */
+  Full height
   height: 100vh;
   width: 100%;
   
 
-  /* Center and scale the image nicely */
+  Center and scale the image nicely
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover; 
+} */
+.box{
+  width: 100px;
+  height: 100px;
+  background-color: #000;
+  position: relative;
+  z-index: 10;
+}
+.bg{
+  position: relative;
+  z-index: -1;
+  height: 100vh;
 }
 .img {
   height: 200px;
@@ -59,7 +89,7 @@ export default {
   line-height:200px;
 }
 .img img{
-  margin-top:150%;
+  /* margin-top:150%; */
   width: 100%;
   height: 150px;
   vertical-align: middle;
@@ -67,5 +97,10 @@ export default {
 }
 div img:hover{
   transform:scale(1.2);
+}
+.row{
+  margin:0 !important;
+  position: relative;
+  z-index: 10;
 }
 </style>
