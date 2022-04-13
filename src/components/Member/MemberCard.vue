0d3aceab0c7366a09a4884436055b671b559d673
zh1x277 <98078345+zh1x277@users.noreply.github.com>
2022-04-13 16:06:22 +0800

    '20220413_member'

--- src/components/Member/MemberCard.vue
+++ src/components/Member/MemberCard.vue
@@ -1,12 +1,5 @@
 <template>
-<div class="scene">
-  <div class="box">
-    <div class="box_face box_face-front">front</div>
-    <div class="box_face box_face-back">back</div>
-  </div>
-</div>
-
- <!-- <div class="col mx-1">
+ <div class="col mx-1">
     <div class="member-bg p-3 card shadow-sm d-flex justify-center h-100 border border-2 border-warning">
         <div class="col h-100">
           <img v-bind:src="src" class="p-2 card-img-top rounded-circle border border-warning"/>
@@ -21,7 +14,7 @@
       </a>
     </div>
     </div>
- </div> -->
+ </div>
 </template>
 
 <script>
@@ -34,12 +27,8 @@ export default {
   }
 }
 
-var box = document.querySelector('.box');
-if(box){
-box.addEventListener( 'click', function() {
-  box.classList.toggle('is-flipped');
-});
-}
+
+
 
 </script>
 
@@ -50,58 +39,15 @@ font-family: jf-openhuninn;
 color: #802300;
 }
 
-/* .flipped{
+.flipped{
   transform: rotateY(180deg);
-} */
-/* 
+} 
+
 .member-bg{
   background-image: url('../../assets/member/member_box_bg.jpg');
   background-repeat:no-repeat;
   background-position:right bottom;
   background-size: contain;
-} */
-
-.scene {
-  width: 300px;
-  height: 500px;
-  border: 1px solid #CCC;
-  margin: 40px 0;
-  perspective: 600px;
-}
-
-.box {
-  width: 100%;
-  height: 100%;
-  transition: transform 1s;
-  transform-style: preserve-3d;
-  cursor: pointer;
-  position: relative;
-}
-
-.box.is-flipped {
-  transform: rotateY(180deg);
-}
-
-.box_face {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  line-height: 260px;
-  color: white;
-  text-align: center;
-  font-weight: bold;
-  font-size: 40px;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
-}
-
-.box_face-front {
-  background: red;
-}
-
-.box_face-back {
-  background: blue;
-  transform: rotateY(180deg);
 }
 
 
